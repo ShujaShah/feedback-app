a1530e573347753a1a44daf7671e36aaf2626ccd
shuja-shah3 <shuja.shah@iquasar.com>
2023-03-14 10:45:10 +0530

    Basic map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-function App(){
-    return(
-        <h1>Hello from the app component</h1>
-    )
+function App() {
+  const title = "Blog Post";
+  const body = "This is the body of the blog post";
+  const comments = [
+    { id: 1, text: "This is the First comment" },
+    { id: 1, text: "This is the Second comment" },
+    { id: 1, text: "This is the Third comment" },
+  ];
+  return (
+    <div className="container">
+      <h1>{title}</h1>
+      <p>{body}</p>
+      <div className="comments">
+        <h3>Comments:({comments.length})</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
