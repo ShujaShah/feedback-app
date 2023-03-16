0d5390b76d63f8ad0ef40e186c883f881b55036e
shuja-shah3 <shuja.shah@iquasar.com>
2023-03-16 11:55:10 +0530

    added the validation to the form

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,8 +5,22 @@ import Button from "./shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
+    if (text === "") {
+      //condition to check if there is no text inputted, set the button to disabled
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text Must be atleast 10 Characters");
+       setBtnDisabled(true);
+    } else {
+      setMessage(null);
+       setBtnDisabled(false);
+    }
+
     setText(e.target.value);
   };
   return (
@@ -16,8 +30,11 @@ function FeedbackForm() {
 
         <div className="input-group">
           <input onChange={handleTextChange} type="text" placeholder="Write your review" value={text}></input>
-          <Button type="submit">Submit</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Submit
+          </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
