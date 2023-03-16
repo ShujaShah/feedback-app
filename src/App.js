23857c94cb23fe6815538288b8de5bd10d642a3a
shuja-shah3 <shuja.shah@iquasar.com>
2023-03-16 22:50:23 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
@@ -5,6 +6,7 @@ import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import { v4 as uuidv4 } from "uuid";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -22,13 +24,27 @@ function App() {
 
   return (
     <>
-      <Header />
+      <Router>
+        <Header />
 
-      <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-      </div>
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackStats feedback={feedback} />
+                  <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                </>
+              }
+            ></Route>
+
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Card from "../components/shared/Card";
+
+function AboutPage() {
+  return (
+    <Card>
+      <h1>About this Project</h1>
+      <p>This is the react app to leave the feedback for the products or services</p>
+      <p>Version: 1.0.0</p>
+      <p>
+        <a href="/">Back to Home</a>
+      </p>
+    </Card>
+  );
+}
+
+export default AboutPage;
