820f49ab4a12caf82c6cfd3b4883cb3cd89d2cb0
shuja-shah3 <shuja.shah@iquasar.com>
2023-03-17 22:06:30 +0530

    added the edit functionality

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,7 +1,7 @@
 import { createContext, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-const FeedbackContext = createContext();
+const FeedbackContext = createContext(); //use context
 
 export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
